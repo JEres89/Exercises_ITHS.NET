69faba60cbc9f2b650bf98cf6edf5797e2ebc904
Jens Eresund <jens.eresund@gmail.com>
2024-08-30 15:33:46 +0200

    Add project files.

---
+++ Exercises_1/Loopar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_1;
+internal class Loopar
+{
+	public Loopar()
+	{
+		Console.WriteLine("asd");
+
+	}
+}
---
+++ Exercises_1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Exercises_1;
+
+internal class Program
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="args"></param>
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello World!");
+
+		// Statements (satser):
+		// 1. Declaration, Assignment
+		int a = 5;
+		int b;
+
+		// 2. Expression statement
+		b = a * a;
+
+		// 3. Method call
+		Console.WriteLine(b);
+
+		// 4. Iteration
+		for(int i = 0 ; i < 10 ; i++)
+		{
+			Console.WriteLine(i);
+		}
+
+		// 5. Selection
+		if(a > 5)
+		{
+			Console.WriteLine("a is greater than 5");
+		}
+		else
+		{
+			Console.WriteLine("a is less than or equal to 5");
+		}
+
+
+	}
+}
